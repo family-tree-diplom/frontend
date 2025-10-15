2b6071a88e708a8f9651d98aac572ed8edf6b0ec
shablovskyi-tuf <shablovskyiillia@gmail.com>
2025-10-15 08:58:17 +0300

    continue refactoring front

--- components/FamilyTree.vue
+++ components/FamilyTree.vue
@@ -302,24 +302,28 @@ const treeLayout = computed(() => {
 // --- Всі вузли і лінки для SVG (reactive) ---
 const allNodes = computed(() => treeLayout.value.nodes);
 
-const peoples = await useAsyncData('peoples', async () => {
-    const response = await $fetch('api/peoples', {
-        baseURL: process.server ? config.public.API_BASE_URL : '',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: {
-            jsonrpc: '2.0',
-            method: 'default',
-            params: {},
-        },
-    });
+const peoples = await useAsyncData(
+    'peoples',
+    async () => {
+        const response = await $fetch('api/peoples', {
+            baseURL: process.server ? config.public.API_BASE_URL : '',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: {
+                jsonrpc: '2.0',
+                method: 'default',
+                params: {},
+            },
+        });
 
-    return response[0].result;
-}, {
-    default: ()=>[],
-});
+        return response[0].result;
+    },
+    {
+        default: () => [],
+    }
+);
 
 const allLinks = computed(() => treeLayout.value.links);
 
@@ -466,70 +470,12 @@ function onPersonClick(node) {
                             </defs>
                             <!-- Лінії (зв'язки) -->
                             <g>
-                                <!--                                <base-line  :peoples="allNodes" :relations="allLinks"></base-line>-->
-                                <template v-for="(link, idx) in allLinks" :key="'link-' + idx">
-                                    <!-- Всі варіанти зв'язків, платформа, шлюб, батьки, діти -->
-                                    <line
-                                        v-if="link.direction === 'marriageToChild'"
-                                        :x1="allNodes.find((n) => n.id === link.from)?.x"
-                                        :y1="allNodes.find((n) => n.id === link.from)?.y"
-                                        :x2="allNodes.find((n) => n.id === link.to)?.x"
-                                        :y2="allNodes.find((n) => n.id === link.to)?.y"
-                                        stroke="#bbb"
-                                        stroke-width="3"
-                                        opacity="0.6"
-                                    />
-                                    <line
-                                        v-else-if="link.direction === 'personToChild'"
-                                        :x1="allNodes.find((n) => n.id === link.from)?.x"
-                                        :y1="allNodes.find((n) => n.id === link.from)?.y"
-                                        :x2="allNodes.find((n) => n.id === link.to)?.x"
-                                        :y2="allNodes.find((n) => n.id === link.to)?.y"
-                                        stroke="#bbb"
-                                        stroke-width="3"
-                                        opacity="0.6"
-                                    />
-                                    <line
-                                        v-else-if="link.direction === 'marriageToPlatform'"
-                                        :x1="link.x"
-                                        :y1="link.y1"
-                                        :x2="link.x"
-                                        :y2="link.y2"
-                                        stroke="#bbb"
-                                        stroke-width="3"
-                                        opacity="0.6"
-                                    />
-                                    <line
-                                        v-else-if="link.direction === 'platform' && Math.abs(link.x2 - link.x1) > 10"
-                                        :x1="link.x1"
-                                        :y1="link.y"
-                                        :x2="link.x2"
-                                        :y2="link.y"
-                                        stroke="#333"
-                                        stroke-width="3"
-                                        opacity="0.9"
-                                    />
-                                    <line
-                                        v-else-if="link.direction === 'platformToChild'"
-                                        :x1="link.x"
-                                        :y1="link.y1"
-                                        :x2="link.x"
-                                        :y2="link.y2"
-                                        stroke="#444"
-                                        stroke-width="3"
-                                        opacity="0.7"
-                                    />
-                                    <line
-                                        v-else
-                                        :x1="allNodes.find((n) => n.id === link.from)?.x"
-                                        :y1="allNodes.find((n) => n.id === link.from)?.y"
-                                        :x2="allNodes.find((n) => n.id === link.to)?.x"
-                                        :y2="allNodes.find((n) => n.id === link.to)?.y"
-                                        stroke="#ccc"
-                                        stroke-width="3"
-                                        opacity="0.7"
-                                    />
-                                </template>
+                                <base-line
+                                    v-for="(link, idx) in allLinks"
+                                    :key="'link-' + idx"
+                                    :relations="link"
+                                    :peoples="allNodes"
+                                ></base-line>
                             </g>
                             <!-- Вузли -->
                             <g>
--- components/base/line.vue
+++ components/base/line.vue
@@ -5,82 +5,89 @@ const props = defineProps({
         required: true,
     },
     relations: {
-        type: Array,
-        default() {
-            return [{
-                direction: '',
-                from: '',
-                to: '',
-                x: '',
-                x1: '',
-                x2: '',
-                y: '',
-                y1: '',
-                y2: '',
-            }];
-        },
+        type: Object,
+        required: true,
     },
+});
+
+// Знаходимо початковий вузол за його ID
+const fromPeoples = computed(() => {
+    if (!props.relations.from) return null;
+    return props.peoples.find((n) => n.id === props.relations.from);
+});
+
+// Знаходимо кінцевий вузол за його ID
+const toPeoples = computed(() => {
+    if (!props.relations.to) return null;
+    return props.peoples.find((n) => n.id === props.relations.to);
 });
 </script>
 
-<template v-for="(link, idx) in relations" :key="'link-' + idx">
-    <!-- Всі варіанти зв'язків, платформа, шлюб, батьки, діти -->
+<template>
+    <!-- Вся логіка v-if/v-else-if для різних типів ліній тепер тут -->
+
+    <!-- Зв'язок від шлюбу до однієї дитини -->
     <line
-        v-if="link?.direction === 'marriageToChild'"
-        :x1="peoples.find((n) => n.id === link?.from)?.x"
-        :y1="peoples.find((n) => n.id === link?.from)?.y"
-        :x2="peoples.find((n) => n.id === link?.to)?.x"
-        :y2="peoples.find((n) => n.id === link?.to)?.y"
+        v-if="relations.direction === 'marriageToChild'"
+        :x1="fromPeoples?.x"
+        :y1="fromPeoples?.y"
+        :x2="toPeoples?.x"
+        :y2="toPeoples?.y"
         stroke="#bbb"
         stroke-width="3"
         opacity="0.6"
     />
+    <!-- Зв'язок від одного з батьків до однієї дитини -->
     <line
-        v-else-if="link?.direction === 'personToChild'"
-        :x1="peoples.find((n) => n.id === link?.from)?.x"
-        :y1="peoples.find((n) => n.id === link?.from)?.y"
-        :x2="peoples.find((n) => n.id === link?.to)?.x"
-        :y2="peoples.find((n) => n.id === link?.to)?.y"
+        v-else-if="relations.direction === 'personToChild'"
+        :x1="fromPeoples?.x"
+        :y1="fromPeoples?.y"
+        :x2="toPeoples?.x"
+        :y2="toPeoples?.y"
         stroke="#bbb"
         stroke-width="3"
         opacity="0.6"
     />
+    <!-- Вертикальна лінія від батьків до платформи -->
     <line
-        v-else-if="link?.direction === 'marriageToPlatform'"
-        :x1="link?.x"
-        :y1="link?.y1"
-        :x2="link?.x"
-        :y2="link?.y2"
+        v-else-if="relations.direction === 'marriageToPlatform'"
+        :x1="relations.x"
+        :y1="relations.y1"
+        :x2="relations.x"
+        :y2="relations.y2"
         stroke="#bbb"
         stroke-width="3"
         opacity="0.6"
     />
+    <!-- Горизонтальна платформа для кількох дітей -->
     <line
-        v-else-if="link?.direction === 'platform' && Math.abs(link?.x2 - link?.x1) > 10"
-        :x1="link?.x1"
-        :y1="link?.y"
-        :x2="link?.x2"
-        :y2="link?.y"
+        v-else-if="relations.direction === 'platform' && Math.abs(relations.x2 - relations.x1) > 10"
+        :x1="relations.x1"
+        :y1="relations.y"
+        :x2="relations.x2"
+        :y2="relations.y"
         stroke="#333"
         stroke-width="3"
         opacity="0.9"
     />
+    <!-- Вертикальна лінія від платформи до дитини -->
     <line
-        v-else-if="link?.direction === 'platformToChild'"
-        :x1="link?.x"
-        :y1="link?.y1"
-        :x2="link?.x"
-        :y2="link?.y2"
+        v-else-if="relations.direction === 'platformToChild'"
+        :x1="relations.x"
+        :y1="relations.y1"
+        :x2="relations.x"
+        :y2="relations.y2"
         stroke="#444"
         stroke-width="3"
         opacity="0.7"
     />
+    <!-- Стандартний зв'язок (наприклад, від особи до вузла шлюбу) -->
     <line
         v-else
-        :x1="peoples.find((n) => n.id === link?.from)?.x"
-        :y1="peoples.find((n) => n.id === link?.from)?.y"
-        :x2="peoples.find((n) => n.id === link?.to)?.x"
-        :y2="peoples.find((n) => n.id === link?.to)?.y"
+        :x1="fromPeoples?.x"
+        :y1="fromPeoples?.y"
+        :x2="toPeoples?.x"
+        :y2="toPeoples?.y"
         stroke="#ccc"
         stroke-width="3"
         opacity="0.7"
